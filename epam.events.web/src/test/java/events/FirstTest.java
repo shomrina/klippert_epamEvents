7a41f19a89d890f348dd91a9e5294dc5f72c6293
Klippert Marina <milagrous@gmail.com>
2021-08-06 21:39:47 +0300

    extract fields for locator and common methods

--- epam.events.web/src/test/java/events/FirstTest.java
+++ epam.events.web/src/test/java/events/FirstTest.java
@@ -15,22 +15,46 @@ public class FirstTest extends BaseTest {
 
     private Logger log = LogManager.getLogger(FirstTest.class);
 
+    private String baseUrl = "https://events.epam.com/";
+    private By eventsBtnLocator = By.cssSelector("a[href='/events']");
+    private By loaderLocator = By.cssSelector("div.evnt-global-loader");
+    private By upcomingEventsBntLocator = By.cssSelector("span.evnt-tab-text.desktop");
+    private By upcomingEventCounterLocator = By.cssSelector("span.evnt-tab-counter");
+    private By eventCardsLocator = By.cssSelector("div.evnt-event-card");
+
     @Test
     public void firstTest() {
-        driver.get("https://events.epam.com/");
+        driver.get(baseUrl);
         log.info("Open main page"); //cookie banner : id = onetrust-banner-sdk   & id = onetrust-accept-btn-handler
 
-        driver.findElement(By.cssSelector("a[href='/events']")).click();
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector("div.evnt-global-loader"))));
+        driver.findElement(eventsBtnLocator).click();
+        waitInvisibilityOf(getWebElement(loaderLocator), 10);
         log.info("Go to Events");
 
-        WebElement upcomingEventsBtn = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("span.evnt-tab-text.desktop")));
-        upcomingEventsBtn.click(); //if ![a.evnt-tab-link].getAttribute("class").contains("active") else она уже нажата...
-        int expectedCount = Integer.parseInt(driver.findElement(By.cssSelector("span.evnt-tab-counter")).getText()) ;
-        List<WebElement> eventCard =  driver.findElements(By.cssSelector("div.evnt-event-card"));
-        Assertions.assertEquals(eventCard.size(), expectedCount);
+        waitElementToBeClickable(upcomingEventsBntLocator, 5).click(); //if ![a.evnt-tab-link].getAttribute("class").contains("active") else она уже нажата...
 
+        int expectedCount = Integer.parseInt(driver.findElement(upcomingEventCounterLocator).getText()) ;
+        List<WebElement> eventCard =  getWebElements(eventCardsLocator);
+
+        Assertions.assertEquals(eventCard.size(), expectedCount);
+    }
+
+    private Boolean waitInvisibilityOf(WebElement webElement, int timeInSec) {
+        WebDriverWait wait = new WebDriverWait(driver, timeInSec);
+        return wait.until(ExpectedConditions.invisibilityOf(webElement));
+    }
+
+    private WebElement waitElementToBeClickable(By locator, int timeInSec){
+        WebDriverWait wait = new WebDriverWait(driver, timeInSec);
+        return wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    private WebElement getWebElement(By locator) {
+        return driver.findElement(locator);
+    }
+
+    private List<WebElement> getWebElements(By locator) {
+        return driver.findElements(locator);
     }
 
 }
