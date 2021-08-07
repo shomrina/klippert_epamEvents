67020f41fc50d9a6a3d2833f484ac9d57ef7ae8b
Klippert Marina <milagrous@gmail.com>
2021-08-07 23:17:44 +0300

    replace methods which working with date to DateHelper class and added descriptions

--- epam.events.web/src/test/java/events/UpcomingEventsTest.java
+++ epam.events.web/src/test/java/events/UpcomingEventsTest.java
@@ -5,18 +5,14 @@ import events.pages.MainPage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.joda.time.DateTime;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-
-import java.text.ParseException;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Locale;
+
+import static events.util.DateHelper.convertStringToDateTime;
+import static events.util.DateHelper.splitPeriodOnDate;
 
 /**
  * UpcomingEventsTest class for testing features linked with upcoming events
@@ -24,7 +20,6 @@ import java.util.Locale;
 public class UpcomingEventsTest extends BaseTest {
 
     private Logger log = LogManager.getLogger(UpcomingEventsTest.class);
-
 
     @Test
     @DisplayName("View upcoming events and verify count")
@@ -55,41 +50,10 @@ public class UpcomingEventsTest extends BaseTest {
             String datePeriodInText = eventsPage.getDatePeriodForEvent(eventCards, i);
             List<String> datesInText = splitPeriodOnDate(datePeriodInText);
             DateTime finishDate = convertStringToDateTime(datesInText.get(1));
+
             Assertions.assertTrue(finishDate.isAfterNow() || finishDate.isEqualNow(),
                     "The events " + i + "shouldn't be in upcoming events list. \nfinishDate = " + finishDate + "\ncurrentDate = " + DateTime.now());
         }
-
     }
-
-    public List<String> splitPeriodOnDate(String period) {
-        //divide string on the two strings with date
-        List<String> datesInText = Arrays.asList(period.split(" - "));
-        log.debug("beginDateInText, datesIntext 0: {}", datesInText.get(0));
-        log.debug("finishDateInText, datesIntext 1: {}", datesInText.get(1));
-        return datesInText;
-    }
-
-    public String addedYearToBeginningDate(String beginDateInText, String finishDateInText) {
-        //convert beginning date to format 'dd mmm yyyy'
-        List<String> beginDateInTextList = Arrays.asList(beginDateInText.split(" "));
-        List<String> finishDateInTextList = Arrays.asList(finishDateInText.split(" "));
-        String convertedBeginDateInText;
-        if (beginDateInTextList.size() == 2) {
-            convertedBeginDateInText = beginDateInText + " " + finishDateInTextList.get(2);
-            log.debug("add year to value 'beginDateInText', new value = {}", convertedBeginDateInText);
-        } else convertedBeginDateInText = beginDateInText;
-        return convertedBeginDateInText;
-    }
-
-    public DateTime convertStringToDateTime(String dateInText) {
-        //convert to date format "dd MMM yyyy", for beginDateInText have to contains year! use addedYearToBeginningDate for convert it before.
-        DateTimeFormatter dateFormat = DateTimeFormat.forPattern("dd MMM yyyy").withLocale(Locale.US);
-        DateTime dateTime = dateFormat.parseDateTime(dateInText);
-        log.debug("convertStringToDateTime, dateTime = {}", dateFormat.print(dateTime));
-        return dateTime;
-    }
-
-
-
 
 }
--- epam.events.web/src/test/java/events/util/DateHelper.java
+++ epam.events.web/src/test/java/events/util/DateHelper.java
@@ -1,6 +1,53 @@
 package events.util;
 
+import events.UpcomingEventsTest;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * DateHelper class works with date using DateTime
+ */
+
 public class DateHelper {
+    private static Logger log = LogManager.getLogger(DateHelper .class);
+
+    /** Returns List of two strings with date from String period.*/
+    public static List<String> splitPeriodOnDate(String period) {
+        //divide string on the two strings with date
+        List<String> datesInText = Arrays.asList(period.split(" - "));
+        log.debug("beginDateInText, datesIntext 0: {}", datesInText.get(0));
+        log.debug("finishDateInText, datesIntext 1: {}", datesInText.get(1));
+        return datesInText;
+    }
+
+    /** Returns converted String date from 'dd MMM' to 'dd MMM yyyy' using value 'year' from finishDateInText */
+    public static String addedYearToBeginningDate(String beginDateInText, String finishDateInText) {
+        //convert beginning date to format 'dd mmm yyyy'
+        List<String> beginDateInTextList = Arrays.asList(beginDateInText.split(" "));
+        List<String> finishDateInTextList = Arrays.asList(finishDateInText.split(" "));
+        String convertedBeginDateInText;
+        if (beginDateInTextList.size() == 2) {
+            convertedBeginDateInText = beginDateInText + " " + finishDateInTextList.get(2);
+            log.debug("add year to value 'beginDateInText', new value = {}", convertedBeginDateInText);
+        } else convertedBeginDateInText = beginDateInText;
+        return convertedBeginDateInText;
+    }
+
+    /** Returns date in the format 'DateTime'. The dateInText converted from String to DateTime */
+    public static DateTime convertStringToDateTime(String dateInText) {
+        //convert to date format "dd MMM yyyy", for beginDateInText have to contains year! use addedYearToBeginningDate for convert it before.
+        DateTimeFormatter dateFormat = DateTimeFormat.forPattern("dd MMM yyyy").withLocale(Locale.US);
+        DateTime dateTime = dateFormat.parseDateTime(dateInText);
+        log.debug("convertStringToDateTime, dateTime = {}", dateFormat.print(dateTime));
+        return dateTime;
+    }
 
 
 
